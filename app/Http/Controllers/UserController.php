82b8d63a4c8355b4d78e122e8926ee9915d7397e
aratislamsafol <aratislamsafol@gmail.com>
2025-10-08 05:21:40 +0600

    user registration successfully

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1,12 +1,27 @@
 <?php
-
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use Exception;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    function UserRegistration(Request $request) {
-        
+    public function UserRegistration(Request $request)
+    {
+        try {
+            User::create($request->input());
+
+            return response()->json([
+                'status'  => 'success',
+                'message' => 'user registration successfully',
+            ]);
+
+        } catch (Exception $e) {
+            return response()->json([
+                'status'  => 'failed',
+                'message' => $e->getMessage(),
+            ], 500);
+        }
     }
 }
